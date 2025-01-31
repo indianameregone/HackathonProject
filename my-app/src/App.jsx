3358c38251ea8c27f67023aa485cf948ec80037d
Juan Meregone <juanmeregone@gmail.com>
2025-01-31 13:15:31 -0300

    First Commit

---
+++ my-app/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Nav from './assets/banner'
+
+function App() {
+
+
+  return (  
+    <>
+      <div className="card">
+          <Nav />
+          <h1>Hackathon App</h1>
+        </div>  
+    </>   
+       
+    
+  )
+}
+
+export default App
