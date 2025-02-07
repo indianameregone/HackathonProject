289c2ce7e69869688feb7d08d1f76af331f9398e
Juan Meregone <juanmeregone@gmail.com>
2025-02-07 15:13:24 -0300

    changes in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import './index.css'
 import Nav from './assets/banner'
 import { AppKitProvider, ConnectButton } from './assets/config'
 
 function App() {
-  return (  
-    <AppKitProvider>
-      <div className="card">
+  return (      
+    <div className="card">
         <Nav />
-        <h1>Hackathon App</h1>
-      </div>
-    </AppKitProvider>
+      <AppKitProvider>
+        <h1>Hackathon App</h1>     
+      </AppKitProvider>
+    </div>
   )
 }
 
--- frontend/src/assets/banner.jsx
+++ frontend/src/assets/banner.jsx
@@ -1,16 +1,17 @@
 import './nav.css'
+import '../index.css'
 import { ConnectButton } from './config'
 function nav() {
 
 
   return (  
     <>
-     <ConnectButton></ConnectButton>
         <div className="Nav-Bar">
           <ul>
             <li>Menu</li>
             <li>Us</li>
             <li>currency</li>
+            <ConnectButton className="conn"/>
           </ul>
         </div>  
     </>   
