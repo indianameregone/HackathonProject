73232a22a905508caadf49f1033f30696371ca2b
Juan Meregone <juanmeregone@gmail.com>
2025-03-05 16:38:09 -0300

    more changes

--- frontend/src/components/swap.jsx
+++ frontend/src/components/swap.jsx
@@ -1,16 +1,19 @@
 import Nav from '../assets/banner'
 import { AppKitProvider } from '../assets/config'
 import useFetch from '../useFetch'
+import { useState } from 'react'
 import '../App.css'
 import '../index.css'
 import Footer from '../assets/footer'
 
 function swap() {  
+    const [symbol,setSymbol] = useState('')
     const {data, loading} = useFetch("https://api.coinlore.net/api/tickers/")    
     const onChangeHandler = (e) => {
       const index = e.target.selectedIndex;
       const el = e.target.childNodes[index]
       const option =  el.getAttribute('price');  
+      setSymbol(option)
       console.log(option)
     }
     console.log(data)
@@ -21,36 +24,36 @@ function swap() {
         <AppKitProvider />
         <Nav />
         <section id='swap-sect'>
-        <h1>Swap</h1>
-        <section>          
-          <fieldset className='main-sect'>
-          <legend>I want to sell:</legend>
-          <input type="number" ></input> 
+          <div className='imgByS'>          
+          </div>
+          <section>          
+            <fieldset className='main-sect'>
+            <legend>I want to sell:</legend>
+            <input type="number" placeholder=' Insert Quantity'></input> 
+            <select className='dropDown' onChange={onChangeHandler}>
+              <option>Select a currency</option>
+              {loading && <h2>Loading....</h2>}
+              {data?.map((user)=>(
+                <option key={user.name} className="container" id={user.name} price={user.price_usd}>
+                  <h2 >{user.symbol}</h2> 
+                </option>         
+                ))}
+          </select>
+            </fieldset>
+            <fieldset className='main-sect'>
+            <legend>I want to buy:</legend>
+            <input type="number" placeholder=' Insert Quantity'></input> 
           <select className='dropDown' onChange={onChangeHandler}>
-            <option>Select a currency</option>
+          <option>Select a currency</option>
             {loading && <h2>Loading....</h2>}
             {data?.map((user)=>(
               <option key={user.name} className="container" id={user.name} price={user.price_usd}>
                 <h2 >{user.symbol}</h2> 
               </option>         
               ))}
-        </select>
-          </fieldset>
-          <fieldset className='main-sect'>
-          <legend>I want to buy:</legend>
-          <input type="number"></input> 
-        <select className='dropDown' onChange={onChangeHandler}>
-        <option>Select a currency</option>
-          {loading && <h2>Loading....</h2>}
-          {data?.map((user)=>(
-            <option key={user.name} className="container" id={user.name} price={user.price_usd}>
-              <h2 >{user.symbol}</h2> 
-            </option>         
-            ))}
-        </select>
-          </fieldset>
-       
-        </section>
+          </select>
+            </fieldset>       
+          </section>
         </section>
         
         <Footer />
