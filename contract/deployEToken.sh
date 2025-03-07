c35b48786873d0c18eb6c1150b762e0b7a245980
luigicallero <luigicallero@gmail.com>
2025-03-08 05:57:34 +1300

    ready to production

--- contract/deployEToken.sh
+++ contract/deployEToken.sh
@@ -5,7 +5,13 @@
 # Contract address: 0x5FC8d32690cc91D4c39d9d3abcBD16989F875707
 
 # Testnet deployment
-forge script script/EToken.s.sol:DeployEToken --rpc-url https://rpc.ankr.com/electroneum_testnet \
+# forge script script/EToken.s.sol:DeployEToken --rpc-url https://rpc.ankr.com/electroneum_testnet \
+#     --account wallet_test \
+#     --broadcast > deployment-output.txt
+# Contract address: 0xcD9b83F9033119cA635f6d5d7e8c74C0cF2a5982
+
+# Production deployment
+forge script script/EToken.s.sol:DeployEToken --rpc-url https://rpc.ankr.com/electroneum/6de45fb72fd9001245d88887a67835d7fa83dda3d3f32b6b05f85eb384689782 \
     --account wallet_test \
-    --broadcast > deployment-output.txt
+    --broadcast > deploymentEtoken-output.txt
 # Contract address: 0xcD9b83F9033119cA635f6d5d7e8c74C0cF2a5982
